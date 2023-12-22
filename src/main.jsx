b9b54a655fcedac0effd5955416df9a6f3c59f05
maruf02 <rashiduzzaman99@gmail.com>
2023-12-22 17:22:06 +0600

    initil setup

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root.jsx";
+import HomePage from "./Components/HomePage/HomePage.jsx";
+import DashBoard from "./Components/DashBoard/DashBoard/DashBoard.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard></DashBoard>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
